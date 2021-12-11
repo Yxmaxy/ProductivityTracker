3ce223fa5a91adfabe634f2a0aa2e09943691213
Yxmaxy <57218072+Yxmaxy@users.noreply.github.com>
2021-12-11 22:24:31 +0100

    Started the design

--- App.js
+++ App.js
@@ -1,21 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Constants from "expo-constants";
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+function HomeScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Home!</Text>
+        </View>
+    );
+}
+  
+  function SettingsScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Settings!</Text>
+        </View>
+    );
+}
+
+const Tab = createMaterialTopTabNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [date, setDate] = useState(null);
+
+    useEffect(() => {
+        const today = new Date;
+        setDate(today.getDate() + ". " + (today.getMonth() + 1) + ". " + today.getFullYear());
+    }, [])
+
+    return (
+        <View style={ styles.container }>
+            <Text style={styles.dateText}>{date}</Text>
+            <NavigationContainer>
+                <Tab.Navigator 
+                    initialRouteName={"Today"}
+                    screenOptions={{
+                        tabBarLabelStyle: { fontSize: 11 },
+                        tabBarStyle: { backgroundColor: 'dodgerblue' },
+                        tabBarInactiveTintColor: "black",
+                        tabBarPressColor: "rgba(255, 255, 255, 0.3)",
+                        tabBarActiveTintColor: "yellow",
+                        tabBarIndicatorStyle: {
+                            backgroundColor: "yellow"
+                        },
+                    }}
+                >
+                    <Tab.Screen name="Today" component={HomeScreen} />
+                    <Tab.Screen name="Month" component={SettingsScreen} />
+                    <Tab.Screen name="Longterm" component={SettingsScreen} />
+                    <Tab.Screen name="Reminders" component={SettingsScreen} />
+                </Tab.Navigator>
+            </NavigationContainer>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    dateText: {
+        backgroundColor: "lime",
+        textAlign: "right",
+        padding: 10,
+        fontWeight: "bold",
+    },
+    container: {
+        flex: 1,
+        paddingTop: Constants.statusBarHeight,
+    },
 });
