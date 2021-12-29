0a60b610b0ca667d4dcd7cde34155105a1b78fbf
Yxmaxy <57218072+Yxmaxy@users.noreply.github.com>
2021-12-29 12:33:26 +0100

    Added coloring to month screen

--- components/screens/MonthScreen.js
+++ components/screens/MonthScreen.js
@@ -1,15 +1,85 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Calendar } from 'react-native-calendars';
+import { currentDate, db } from '../common/globals';
+import { colors } from '../common/styles';
 
 const MonthScreen = () => {
-    useEffect(() => {
-        // get previous results
-    }, [])
-    
-    const currentDate = new Date().toISOString().split('T')[0];
+    const [markedDates, setMarkedDates] = useState({});
 
-    const markedDates = {};
-    markedDates[currentDate] = { color: "dodgerblue", textColor: "white" }
+    useEffect(() => {
+        setMarkedDates({
+            ...markedDates,
+            [currentDate]: { color: colors.colorPopBlue, textColor: "white" },
+        });
+
+        const todayDate = new Date().getDate();
+        
+        for (let day = 1; day < todayDate; day++) {
+            const lookedAtDate = new Date().getFullYear() + "-" + (new Date().getMonth() + 1) + "-" + day;
+            let percentage = 0;
+            db.transaction(tx => {
+                tx.executeSql(`
+                    SELECT COUNT(*) AS finishedGoalsNum FROM (
+                        SELECT "0" AS type, COUNT(*) AS count
+                        FROM GoalWeek JOIN Goals AS g USING(id_goal) JOIN GoalFinished USING(id_goal)
+                        WHERE strftime('%w', '${lookedAtDate}') = day
+                        AND DATE('${lookedAtDate}') >= date_started
+                        UNION
+                        SELECT "1" AS type, COUNT(*) AS count
+                        FROM GoalMonth JOIN Goals AS g USING(id_goal) JOIN GoalFinished USING(id_goal)
+                        WHERE CAST(strftime('%d', '${currentDate}') AS Integer) BETWEEN day - num_available_before AND day
+                        AND DATE('${currentDate}') >= date_started
+                        UNION
+                        SELECT "2" AS type, COUNT(*) AS count
+                        FROM GoalYear JOIN Goals AS g USING(id_goal) JOIN GoalFinished USING(id_goal)
+                        WHERE substr(DATE('${currentDate}'), 6) BETWEEN substr(DATE(date, '-' || num_available_before || ' days'), 6) AND substr(date, 6)
+                        AND DATE('${currentDate}') >= date_started
+                        UNION
+                        SELECT "3" AS type, COUNT(*) AS count
+                        FROM GoalCustom JOIN Goals AS g USING(id_goal) JOIN GoalFinished USING(id_goal)
+                        WHERE ((round(julianday(strftime('%Y-%m-%d', '${currentDate}')) - julianday(first_date))) % num_days_between) = 0 OR ((round(julianday(strftime('%Y-%m-%d', '${currentDate}')) - julianday(first_date))) % num_days_between) >= num_days_between - num_available_before
+                        AND DATE('${currentDate}') >= date_started
+                    );
+                `, [], (_, { rows }) => {
+                    percentage = rows._array[0].finishedGoalsNum;
+                }, (t, error) => {
+                    console.log(error);
+                });
+                tx.executeSql(`
+                    SELECT COUNT(*) AS allGoalsNum FROM (
+                        SELECT id_goal
+                        FROM GoalWeek JOIN Goals AS g USING(id_goal) JOIN GoalGroups USING(id_group)
+                        WHERE strftime('%w', '${lookedAtDate}') = day
+                        AND DATE('${lookedAtDate}') >= date_started
+                        UNION
+                        SELECT id_goal
+                        FROM GoalMonth JOIN Goals AS g USING(id_goal) JOIN GoalGroups USING(id_group)
+                        WHERE CAST(strftime('%d', '${currentDate}') AS Integer) BETWEEN day - num_available_before AND day
+                        AND DATE('${currentDate}') >= date_started
+                        UNION
+                        SELECT id_goal
+                        FROM GoalYear JOIN Goals AS g USING(id_goal) JOIN GoalGroups USING(id_group)
+                        WHERE substr(DATE('${currentDate}'), 6) BETWEEN substr(DATE(date, '-' || num_available_before || ' days'), 6) AND substr(date, 6)
+                        AND DATE('${currentDate}') >= date_started
+                        UNION
+                        SELECT id_goal
+                        FROM GoalCustom JOIN Goals AS g USING(id_goal) JOIN GoalGroups USING(id_group)
+                        WHERE ((round(julianday(strftime('%Y-%m-%d', '${currentDate}')) - julianday(first_date))) % num_days_between) = 0 OR ((round(julianday(strftime('%Y-%m-%d', '${currentDate}')) - julianday(first_date))) % num_days_between) >= num_days_between - num_available_before
+                        AND DATE('${currentDate}') >= date_started
+                    );
+                `, [], (_, { rows }) => {
+                    percentage /= rows._array[0].allGoalsNum;
+                    console.log(percentage);
+                    setMarkedDates(prevState => ({
+                        ...prevState,
+                        [lookedAtDate]: { color: `hsl(${parseInt(percentage * 100)}, 85%, 50%)`, textColor: "black" }
+                    }));
+                }, (t, error) => {
+                    console.log(error);
+                });
+            });   
+        }
+    }, []);
 
     return (
         <>
