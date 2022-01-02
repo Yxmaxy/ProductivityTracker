edf1fa0084aafa25d77b487a56702f756858a0e2
Yxmaxy <57218072+Yxmaxy@users.noreply.github.com>
2022-01-02 21:46:11 +0100

    Goals are now presented in groups

--- components/screens/LongtermScreen.js
+++ components/screens/LongtermScreen.js
@@ -5,18 +5,21 @@ import FloatingButton from "../FloatingButton";
 import { Context } from "../common/Store";
 import { flexStyles, floatingButtonStyles } from "../common/styles";
 import { currentDate, db } from "../common/globals";
+import GoalGroup from "../GoalGroup";
 
 const LongtermScreen = ({ navigation }) => {
     const [goals, setGoals] = useState([]);
     const [smallerGoals, setSmallerGoals] = useState({});
+    const [goalsByGroup, setGoalsByGroup] = useState({});
 
     const [storeState, ] = useContext(Context);
 
+    //* effect to get longterm goals
     useEffect(() => {
         db.transaction((tx) => {
             tx.executeSql(`
-                SELECT g.*, color
-                FROM Goals AS g JOIN GoalGroups USING(id_group)
+                SELECT g.*, color, gr.name AS group_name
+                FROM Goals AS g JOIN GoalGroups AS gr USING(id_group)
                 WHERE is_longterm;
             `, [], (_, { rows }) => {
                 setGoals(rows._array);
@@ -26,6 +29,16 @@ const LongtermScreen = ({ navigation }) => {
         });
     }, [storeState.forceUpdate]);
 
+    //* effect for sorting goals by groups
+    useEffect(() => {
+        setGoalsByGroup(goals.reduce((r, a) => {
+            r[a.group_name] = r[a.group_name] || [];
+            r[a.group_name].push(a);
+            return r;
+        }, Object.create(null)));
+    }, [goals]);
+
+    //* effect for smaller goals
     useEffect(() => {
         var goalIDs = [];
         goals.forEach(goal => {
@@ -47,17 +60,22 @@ const LongtermScreen = ({ navigation }) => {
     return (
         <>
             <ScrollView style={ flexStyles.container }>
-                {goals.map(goal => {
+                {Object.entries(goalsByGroup).map((group) => {
                     return (
-                        <View key={goal.id_goal}>
-                            <Goal
-                                id={goal.id_goal}
-                                title={goal.name}
-                                color={goal.color}
-                                smallerGoals={smallerGoals[goal.id_goal]}
-                                endingDate={currentDate}
-                            />
-                        </View>
+                        <GoalGroup key={group[0]} title={group[0]}>
+                            {group[1].map(goal => {
+                                return (
+                                    <Goal
+                                        key={goal.id_goal}
+                                        id={goal.id_goal}
+                                        title={goal.name}
+                                        color={goal.color}
+                                        smallerGoals={smallerGoals[goal.id_goal]}
+                                        endingDate={currentDate}
+                                    />
+                                );
+                            })}
+                        </GoalGroup>
                     );
                 })}
                 <View style={floatingButtonStyles.containerWithMargin} />
--- components/screens/TodayScreen.js
+++ components/screens/TodayScreen.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { ScrollView, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 import Goal from "../Goal";
 import FloatingButton from "../FloatingButton";
 import { Context } from "../common/Store";
@@ -13,6 +13,7 @@ const TodayScreen = ({ navigation }) => {
     const [goalsYear, setGoalsYear] = useState([]);
     const [goalsCustom, setGoalsCustom] = useState([]);
     const [smallerGoals, setSmallerGoals] = useState({});
+    const [goalsByGroup, setGoalsByGroup] = useState({});
 
     const [storeState, ] = useContext(Context);
 
@@ -21,8 +22,8 @@ const TodayScreen = ({ navigation }) => {
         db.transaction((tx) => {
             // Weekly goals
             tx.executeSql(`
-                    SELECT g.*, color
-                    FROM GoalWeek JOIN Goals AS g USING(id_goal) JOIN GoalGroups USING(id_group)
+                    SELECT g.*, color, gr.name AS group_name
+                    FROM GoalWeek JOIN Goals AS g USING(id_goal) JOIN GoalGroups AS gr USING(id_group)
                     WHERE strftime('%w', '${currentDate}') = day
                     AND DATE('${currentDate}') >= date_started
                     ORDER BY id_group;
@@ -34,8 +35,8 @@ const TodayScreen = ({ navigation }) => {
             });
             // Monthly goals
             tx.executeSql(`
-                    SELECT g.*, color, day, (strftime('%Y-%m-','${currentDate}') || day) AS endingDate
-                    FROM GoalMonth JOIN Goals AS g USING(id_goal) JOIN GoalGroups USING(id_group)
+                    SELECT g.*, color, gr.name AS group_name, day, (strftime('%Y-%m-','${currentDate}') || day) AS endingDate
+                    FROM GoalMonth JOIN Goals AS g USING(id_goal) JOIN GoalGroups AS gr USING(id_group)
                     WHERE CAST(strftime('%d', '${currentDate}') AS Integer) BETWEEN day - num_available_before AND day
                     AND DATE('${currentDate}') >= date_started
                     ORDER BY id_group;
@@ -47,9 +48,9 @@ const TodayScreen = ({ navigation }) => {
             });
             // Yearly goals
             tx.executeSql(`
-                    SELECT g.*, color, date, (strftime('%Y-','${currentDate}') || strftime('%m-%d', date)) AS endingDate,
+                    SELECT g.*, color, gr.name AS group_name, date, (strftime('%Y-','${currentDate}') || strftime('%m-%d', date)) AS endingDate,
                     substr(DATE(date, '-' || num_available_before || ' days'), 6) AS dateDiff
-                    FROM GoalYear JOIN Goals AS g USING(id_goal) JOIN GoalGroups USING(id_group)
+                    FROM GoalYear JOIN Goals AS g USING(id_goal) JOIN GoalGroups AS gr USING(id_group)
                     WHERE substr(DATE('${currentDate}'), 6) BETWEEN dateDiff AND substr(date, 6)
                     AND DATE('${currentDate}') >= date_started
                     ORDER BY id_group;
@@ -61,10 +62,10 @@ const TodayScreen = ({ navigation }) => {
             });
             // Custom goals
             tx.executeSql(`
-                    SELECT g.*, color, first_date, num_days_between, num_available_before,
+                    SELECT g.*, color, gr.name AS group_name, first_date, num_days_between, num_available_before,
                     strftime("%Y-%m-%d", julianday('${currentDate}', '+' || num_days_between || ' days', '-' || ((round(julianday('${currentDate}') - julianday(first_date))) % num_days_between) || ' days')) AS endingDate,
                     ((round(julianday('${currentDate}') - julianday(first_date))) % num_days_between) AS dateDiff
-                    FROM GoalCustom JOIN Goals AS g USING(id_goal) JOIN GoalGroups USING(id_group)
+                    FROM GoalCustom JOIN Goals AS g USING(id_goal) JOIN GoalGroups AS gr USING(id_group)
                     WHERE dateDiff = 0 OR dateDiff >= num_days_between - num_available_before
                     AND DATE('${currentDate}') >= date_started
                     ORDER BY id_group;
@@ -76,6 +77,16 @@ const TodayScreen = ({ navigation }) => {
             });
         });
     }, [storeState.forceUpdate]);
+
+    //* effect for sorting goals by groups
+    useEffect(() => {
+        const allGoals = goalsWeek.concat(goalsMonth).concat(goalsYear).concat(goalsCustom);
+        setGoalsByGroup(allGoals.reduce((r, a) => {
+            r[a.group_name] = r[a.group_name] || [];
+            r[a.group_name].push(a);
+            return r;
+        }, Object.create(null)));
+    }, [goalsWeek, goalsMonth, goalsYear, goalsCustom]);
 
     //* effect for smaller goals
     useEffect(() => {
@@ -108,62 +119,24 @@ const TodayScreen = ({ navigation }) => {
     return (
         <>
             <ScrollView>
-                <GoalGroup title="Weekly goals">
-                    {goalsWeek.map(goal => {
-                        return (
-                            <Goal
-                                key={goal.id_goal}
-                                id={goal.id_goal}
-                                title={goal.name}
-                                color={goal.color}
-                                smallerGoals={smallerGoals[goal.id_goal]}
-                                endingDate={currentDate}
-                            />
-                        );
-                    })}
-                </GoalGroup>
-                <GoalGroup title="Monthly goals">
-                    {goalsMonth.map(goal => {
-                        return (
-                            <Goal
-                                key={goal.id_goal}
-                                id={goal.id_goal}
-                                title={goal.name}
-                                color={goal.color}
-                                smallerGoals={smallerGoals[goal.id_goal]}
-                                endingDate={goal.endingDate}
-                            />
-                        );
-                    })}
-                </GoalGroup>
-                <GoalGroup title="Yearly goals">
-                    {goalsYear.map(goal => {
-                        return (
-                            <Goal
-                                key={goal.id_goal}
-                                id={goal.id_goal}
-                                title={goal.name}
-                                color={goal.color}
-                                smallerGoals={smallerGoals[goal.id_goal]}
-                                endingDate={goal.endingDate}
-                            />
-                        );
-                    })}
-                </GoalGroup>
-                <GoalGroup title="Custom goals">
-                    {goalsCustom.map(goal => {
-                        return (
-                            <Goal
-                                key={goal.id_goal}
-                                id={goal.id_goal}
-                                title={goal.name}
-                                color={goal.color}
-                                smallerGoals={smallerGoals[goal.id_goal]}
-                                endingDate={goal.endingDate}
-                            />
-                        );
-                    })}
-                </GoalGroup>
+                {Object.entries(goalsByGroup).map((group) => {
+                    return (
+                        <GoalGroup title={group[0]}>
+                            {group[1].map(goal => {
+                                return (
+                                    <Goal
+                                        key={goal.id_goal}
+                                        id={goal.id_goal}
+                                        title={goal.name}
+                                        color={goal.color}
+                                        smallerGoals={smallerGoals[goal.id_goal]}
+                                        endingDate={currentDate}
+                                    />
+                                );
+                            })}
+                        </GoalGroup>
+                    );
+                })}
                 <View style={floatingButtonStyles.containerWithMargin} />
             </ScrollView>
             <FloatingButton text="+" navigation={navigation} navigateTo="AddGoal" />
